307fb159616d4f152d77a739ae19fc327fd310bc
Danilo da Silva Souza <danilosilva441@gmail.com>
2021-12-30 00:01:07 -0300

    Movie DTO, Controller, Repository e Service

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,10 +7,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_genre (name) VALUES ('Ação');
